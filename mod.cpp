88f1a070ea775e37e3a9bc74639c17f04991d2cf
kentakunkentakun <kenta.0602.2000@icloud.com>
2025-12-04 03:48:59 +0900

    kk

--- mod.cpp
+++ mod.cpp
@@ -1,10 +1,14 @@
 
 // a^n mod を計算する
 
-long long modpow(long long a, long long n, long long mod) {
+long long modpow(long long a, long long n, long long mod)
+{
+    a %= mod;
     long long res = 1;
-    while (n > 0) {
-        if (n & 1) res = res * a % mod;
+    while (n > 0)
+    {
+        if (n & 1)
+            res = res * a % mod;
         a = a * a % mod;
         n >>= 1;
     }
@@ -13,44 +17,55 @@ long long modpow(long long a, long long n, long long mod) {
 
 // a^{-1} mod を計算する
 
-long long modinv(long long a, long long mod) {
+long long modinv(long long a, long long mod)
+{
     return modpow(a, mod - 2, mod);
 }
 /////////////////////////////////////////
-//拡張ユークリッドの互除法
-//mがmod 
-long long modinv(long long a, long long m) {
+// 拡張ユークリッドの互除法
+// mがmod
+long long modinv(long long a, long long m)
+{
     long long b = m, u = 1, v = 0;
-    while (b) {
+    while (b)
+    {
         long long t = a / b;
-        a -= t * b; swap(a, b);
-        u -= t * v; swap(u, v);
+        a -= t * b;
+        swap(a, b);
+        u -= t * v;
+        swap(u, v);
     }
-    u %= m; 
-    if (u < 0) u += m;
+    u %= m;
+    if (u < 0)
+        u += m;
     return u;
 }
 ////////////////////////////////////////
-//二項係数 nCr
+// 二項係数 nCr
 const int MAX = 700000;
 long long fac[MAX], finv[MAX], inv[MAX];
 
 // テーブルを作る前処理
-void COMinit() {
+void COMinit()
+{
     fac[0] = fac[1] = 1;
     finv[0] = finv[1] = 1;
     inv[1] = 1;
-    for (int i = 2; i < MAX; i++){
+    for (int i = 2; i < MAX; i++)
+    {
         fac[i] = fac[i - 1] * i % MOD;
-        inv[i] = MOD - inv[MOD%i] * (MOD / i) % MOD;
+        inv[i] = MOD - inv[MOD % i] * (MOD / i) % MOD;
         finv[i] = finv[i - 1] * inv[i] % MOD;
     }
 }
 
 // 二項係数計算
-long long COM(int n, int k){
-    if (n < k) return 0;
-    if (n < 0 || k < 0) return 0;
+long long COM(int n, int k)
+{
+    if (n < k)
+        return 0;
+    if (n < 0 || k < 0)
+        return 0;
     return fac[n] * (finv[k] * finv[n - k] % MOD) % MOD;
 }
 //////////////////////////////////////
